83887f20258a90ee4b8f8502a7614338e121b467
liam <liamoh@outlook.com>
2017-03-14 15:06:06 +0000

    Adds bar chart to authentication activity as part of logo. Adds MP-Android dependency.

--- app/src/main/java/com/loh/tally/ui/authentication/activity/AuthenticationActivity.java
+++ app/src/main/java/com/loh/tally/ui/authentication/activity/AuthenticationActivity.java
@@ -7,6 +7,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.loh.tally.R;
 import com.loh.tally.ui.authentication.adapter.AuthenticationPagerAdapter;
 import com.loh.tally.ui.authentication.event.AuthenticationChoiceEvent;
@@ -22,6 +28,9 @@ import com.loh.tally.ui.main.activity.MainActivity;
 import com.loh.tally.util.IntentUtil;
 import com.squareup.otto.Subscribe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import butterknife.BindView;
@@ -30,6 +39,7 @@ public class AuthenticationActivity extends BaseActivity implements Authenticati
 
     @BindView(R.id.viewpager) ViewPager viewPager;
     @BindView(R.id.container) View container;
+    @BindView(R.id.barChart) BarChart barChart;
 
     @Inject AuthenticationContract.Presenter presenter;
     @Inject AuthenticationPagerAdapter pagerAdapter;
@@ -46,6 +56,7 @@ public class AuthenticationActivity extends BaseActivity implements Authenticati
         setContentView(R.layout.activity_authentication);
         presenter.attach(this);
         setupViewPager();
+        setupBarChart();
 
         presenter.checkAuthenticationState();
     }
@@ -118,5 +129,54 @@ public class AuthenticationActivity extends BaseActivity implements Authenticati
         Bundle bundle = new Bundle();
         startActivity(MainActivity.getStartingIntent(this, bundle));
         finish();
+    }
+
+    private void setupBarChart() {
+        BarData barData = new BarData(getStartingDataSet());
+        barChart.setData(barData);
+        setupBarDesign();
+    }
+
+    private void setupBarDesign() {
+        barChart.setDrawBorders(false);
+        barChart.getXAxis().setDrawAxisLine(false);
+        barChart.getAxisRight().setDrawAxisLine(false);
+        barChart.getAxisLeft().setDrawAxisLine(false);
+        barChart.setDrawGridBackground(false);
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.getAxisLeft().setDrawGridLines(false);
+        barChart.getAxisRight().setDrawGridLines(false);
+        barChart.getLegend().setEnabled(false);
+        barChart.getXAxis().setDrawLabels(false);
+        barChart.getAxisLeft().setDrawLabels(false);
+        barChart.getAxisRight().setDrawLabels(false);
+        barChart.getDescription().setEnabled(false);
+        barChart.setClickable(false);
+        barChart.setDrawValueAboveBar(false);
+        barChart.getBarData().setDrawValues(false);
+        barChart.setTouchEnabled(false);
+
+    }
+
+    private IBarDataSet getStartingDataSet() {
+        List<BarEntry> yVals = new ArrayList<>();
+        yVals.add(new BarEntry(0, 40));
+        yVals.add(new BarEntry(1, 60));
+        yVals.add(new BarEntry(2, 90));
+        yVals.add(new BarEntry(3, 70));
+        yVals.add(new BarEntry(4, 30));
+
+        BarDataSet dataSet;
+
+        dataSet = new BarDataSet(yVals, "Auth");
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+        dataSets.add(dataSet);
+
+        BarData data = new BarData(dataSets);
+        data.setBarWidth(0.7f);
+        barChart.setData(data);
+
+        return dataSet;
     }
 }
